08dc42df639912a032278a21d2b6a250c88e9709
José Antonio Tlalpa Potrero <antonio@MacBook-Air-de-Jose.local>
2023-03-13 23:38:39 -0600

    Agrego imagen principal y estructura de carrusel

--- lib/pages/PageHome.dart
+++ lib/pages/PageHome.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:card_swiper/card_swiper.dart';
 
 class PageHome extends StatelessWidget {
   const PageHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Inicio", style: TextStyle(fontSize: 30),),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            //padding: EdgeInsets.all(10.0),
+            child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Portada_de_San_Pablo_del_Monte%2C_Tlaxcala_02.jpg/4096px-Mapcarta.jpg"),
+          ),
+          Container(
+            child: _swiper(),
+          ),
+        ],
+      ),
     );
   }
+}
+
+Widget _swiper() {
+  return Container(
+    width: double.infinity,
+    height: 250.0,
+    child: Swiper(
+      viewportFraction: 0.8,
+      scale: 0.9,
+      itemBuilder: (BuildContext context,int index){
+        return Image.network("https://via.placeholder.com/350x150",fit: BoxFit.fill,);
+      },
+      itemCount: 3,
+      pagination: SwiperPagination(),
+      control: SwiperControl(),
+    ),
+  );
 }
